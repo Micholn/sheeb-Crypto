a5a7a15d4f60855c9f7046a6a24b46563e7421aa
Micholn <mdiagbare@gmail.com>
2024-04-14 23:58:07 +0100

    re arranged the app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React log
+        Click on the Vite and React l
       </p>
     </>
   )
