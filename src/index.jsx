effdfd5dbe2580ff3035a8a95e07987ecd508eff
Micholn <mdiagbare#gmail.com>
2024-05-03 23:06:45 +0100

    added the webvitals

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
+import "@fortawesome/fontawesome-free/js/all";
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.jsx'
 import './index.css'
+import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
+reportWebVitals();
