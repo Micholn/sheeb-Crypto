58b11f97c8ef0ac4426e8a0087c0031d3f47275c
Micholn <mdiagbare#gmail.com>
2024-05-06 00:13:40 +0100

    created the database connection logic

--- server/config/db.js
+++ server/config/db.js
@@ -1,2 +1,21 @@
 const mongoose = require('mongoose');
-const { mongoUrl }
+const { MongoUrl } = require('./config');
+
+const connectDB = async() => {
+    try {
+       await mongoose.connect(MongoUrl, {
+         useNewUrlParser: true,
+         useCreateIndex: true,
+         useFindAndModify: false, 
+         useUnifiedTopology: true
+       });
+
+       console.log('MongoDB Connected...');
+    } catch (err) {
+      console.log(error.message);
+      //Exit process with failure 
+      process.exit(1);
+    }
+};
+
+module.exports = connectDB;
