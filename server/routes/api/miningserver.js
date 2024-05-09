ddd5da961e0c35ac00f7aefe70b3b6f4f75ab217
Micholn <mdiagbare#gmail.com>
2024-05-09 23:27:39 +0100

    update on mining server

--- server/routes/api/miningserver.js
+++ server/routes/api/miningserver.js
@@ -12,7 +12,7 @@ router.post(
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ })
+            return res.status(400).json({ errors: errors.array() });
         }
     }
 
