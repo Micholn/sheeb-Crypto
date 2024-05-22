0205b117a0d50d175aa9e212bc8df334ccafc751
Micholn <mdiagbare#gmail.com>
2024-05-22 17:20:39 +0100

    added the avatar request

--- server/routes/api/miningserver.js
+++ server/routes/api/miningserver.js
@@ -261,18 +261,26 @@ router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
 // @route    POST api/posts/comment/:id
 // @desc     Comment on a post
 // @access   Private
-// router.post(
-//   '/comment/:id',
-//   auth,
+ router.post(
+   '/comment/:id',
+   auth,
 //   checkObjectId('id'),
-//   check('text', 'Text is required').notEmpty(),
-//   async (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
+   check('text', 'Text is required').notEmpty(),
+   async (req, res) => {
+     const errors = validationResult(req);
+     if (!errors.isEmpty()) {
+       return res.status(400).json({ errors: errors.array() });
+     }
 
-//     try {
-//       const user = await User.findById(req.user.id).select('-password');
-//       const post =
+     try {
+       const user = await User.findById(req.user.id).select('-password');
+       const post = await Post.findById(req.params.id);
+
+       const newComment = {
+        text: req.body.text, 
+        name: user.name, 
+        avatar: 
+       } 
+
+
 module.exports = router;
