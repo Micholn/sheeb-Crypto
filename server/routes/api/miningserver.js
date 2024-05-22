c2ffafe0e3390c0673e9cac97f3cfffba4c6cc02
Micholn <mdiagbare#gmail.com>
2024-05-22 21:44:36 +0100

    added the server response to the mining server

--- server/routes/api/miningserver.js
+++ server/routes/api/miningserver.js
@@ -288,6 +288,11 @@ router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
        await post.save();
 
        res.json(post.comments);
+     } catch (err) {
+       console.error(err.message);
+       res.status(500).send('Server Error')
+     }
+    }
 );       
 
 
