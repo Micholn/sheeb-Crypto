9fa9e2a07b34400ad88c81ab09ed1f4c5be13907
Micholn <mdiagbare#gmail.com>
2024-05-27 23:18:13 +0100

    further restructuring the faq

--- server/models/Faq.js
+++ server/models/Faq.js
@@ -2,36 +2,3 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
-const Faq = require('../../models/Faq');
-
-//@route Post api/faq
-//@desc Create a faq
-//@access Private 
-router.post(
-  '/',
-  check('question', 'question is required').notEmpty(),
-  check('answer', 'answer is required').notEmpty(),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-      const newFaq = new Faq({
-        question: req.body.question,
-        answer: req.body.answer,
-      });
-
-      const faq = await newFaq.save();
-      res.json(faq);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  }
-);
-
-
-// @route    GET api/faq
-// @desc     Get all faqs
-// @access   Private
