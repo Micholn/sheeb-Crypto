c485802a60f7121432e86ff4360745f0fded751f
Micholn <mdiagbare#gmail.com>
2024-05-27 23:10:37 +0100

    added password to the models

---
+++ server/models/Password.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Schema = new Schema({
+    value: {
+      type: String,
+      default: defaultPassword,
+      required: true
+    }
+  });
+  
